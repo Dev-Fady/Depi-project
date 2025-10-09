5a34454f7265156f2ac831c3f94c9176c0c157d8
Dev-Fady <fadymobiledev@gmail.com>
2025-10-09 18:24:05 +0300

    Set up API application with SQL Server database connection using Entity Framework Core

---
+++ Program.cs
@@ -0,0 +1,42 @@
+
+using AqarakDB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AqarakDB
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            builder.Services.AddDbContext<AppDbContext>(op =>
+            {
+                op.UseSqlServer(builder.Configuration.GetConnectionString("cs"));
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
