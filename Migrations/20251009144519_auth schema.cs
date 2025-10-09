5c5d7d528bb1a168ec07245c70ab322e29892c82
Dev-Fady <fadymobiledev@gmail.com>
2025-10-09 18:29:39 +0300

    Add authentication schema migration and update billing model
    This commit introduces a new migration for the authentication schema, creating `Roles` and `Users` tables with appropriate fields and foreign key constraints. Indexes are added for the `Users` table to optimize queries.
    
    Additionally, the billing schema is updated to include `Payments`, `Roles`, `Subscriptions`, and `Users` entities, establishing necessary foreign key relationships and navigation properties to ensure proper management of these relationships in the entity framework.

---
+++ Migrations/20251009144519_auth schema.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AqarakDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class authschema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "auth");
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                schema: "auth",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RoleType = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                schema: "auth",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RoleId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ParentUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalSchema: "auth",
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Users_Users_ParentUserId",
+                        column: x => x.ParentUserId,
+                        principalSchema: "auth",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_ParentUserId",
+                schema: "auth",
+                table: "Users",
+                column: "ParentUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Phone",
+                schema: "auth",
+                table: "Users",
+                column: "Phone");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RoleId",
+                schema: "auth",
+                table: "Users",
+                column: "RoleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users",
+                schema: "auth");
+
+            migrationBuilder.DropTable(
+                name: "Roles",
+                schema: "auth");
+        }
+    }
+}
