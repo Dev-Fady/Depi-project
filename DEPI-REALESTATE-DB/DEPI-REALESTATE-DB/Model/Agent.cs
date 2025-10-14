8cbe857e99e1cdee98c9b78b6d5179c1c720fbaa
Dev-Fady <fadymobiledev@gmail.com>
2025-10-14 18:56:23 +0300

    Add Agent class and configuration for EF Core

---
+++ DEPI-REALESTATE-DB/DEPI-REALESTATE-DB/Model/Agent.cs
@@ -0,0 +1,10 @@
+﻿namespace DEPI_REALESTATE_DB.Model
+{
+    public class Agent : User
+    {
+        public string AgencyName { get; set; }
+        public int TaxIdentificationNumber { get; set; }
+        public float Rating { get; set; }
+        public int ExperienceYears { get; set; }
+    }
+}
