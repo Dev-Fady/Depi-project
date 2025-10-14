5e7be0602dcd26a4dbfbc6767eb9d0b2246d3128
Dev-Fady <fadymobiledev@gmail.com>
2025-10-14 16:45:12 +0300

    Add AppDbContext and configure database in Program.cs

---
+++ DEPI-REALESTATE-DB/DEPI-REALESTATE-DB/Model/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DEPI_REALESTATE_DB.Model
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base() { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ DEPI-REALESTATE-DB/DEPI-REALESTATE-DB/Program.cs
@@ -0,0 +1,43 @@
+
+using DEPI_REALESTATE_DB.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DEPI_REALESTATE_DB
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            builder.Services.AddDbContext<AppDbContext>(op =>
+            {
+                op.UseSqlServer(builder.Configuration.GetConnectionString("cs"));
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
