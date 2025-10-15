36f19d437d20186db7022d4ccccb6f599d4bf5ae
Dev-Fady <fadymobiledev@gmail.com>
2025-10-15 17:31:53 +0300

    add seed data for Role

--- DEPI-REALESTATE-DB/DEPI-REALESTATE-DB/Model/Config/RoleConfiguration.cs
+++ DEPI-REALESTATE-DB/DEPI-REALESTATE-DB/Model/Config/RoleConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DEPI_REALESTATE_DB.Model.Enums;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace DEPI_REALESTATE_DB.Model.Config
@@ -22,6 +23,30 @@ namespace DEPI_REALESTATE_DB.Model.Config
                    .WithOne(u => u.Role)
                    .HasForeignKey(u => u.RoleId)
                    .OnDelete(DeleteBehavior.Restrict);
+
+            ////  Seed Data
+            //builder.HasData(
+            //    new Role
+            //    {
+            //        Id = Guid.Parse("11111111-1111-1111-1111-111111111111"),
+            //        RoleType = UserRole.Admin
+            //    },
+            //    new Role
+            //    {
+            //        Id = Guid.Parse("22222222-2222-2222-2222-222222222222"),
+            //        RoleType = UserRole.Agent
+            //    },
+            //    new Role
+            //    {
+            //        Id = Guid.Parse("33333333-3333-3333-3333-333333333333"),
+            //        RoleType = UserRole.Broker
+            //    },
+            //    new Role
+            //    {
+            //        Id = Guid.Parse("44444444-4444-4444-4444-444444444444"),
+            //        RoleType = UserRole.Customer
+            //    }
+            //);
         }
     }
 }
