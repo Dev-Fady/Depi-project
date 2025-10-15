4a9c6f40b40c55763dced33ab3967804eb87fa81
Dev-Fady <fadymobiledev@gmail.com>
2025-10-15 18:48:47 +0300

    add ResidentialProperty and Configure

---
+++ DEPI-REALESTATE-DB/DEPI-REALESTATE-DB/Model/Config/ResidentialPropertyConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DEPI_REALESTATE_DB.Model.Config
+{
+    public class ResidentialPropertyConfiguration : IEntityTypeConfiguration<ResidentialProperty>
+    {
+        public void Configure(EntityTypeBuilder<ResidentialProperty> builder)
+        {
+            //builder.HasKey(rp => rp.PropertyId);
+
+            builder.Property(rp => rp.Bedrooms)
+                   .IsRequired();
+
+            builder.Property(rp => rp.Bathrooms)
+                   .IsRequired();
+
+            builder.Property(rp => rp.Floors)
+                   .IsRequired(false);
+
+            builder.Property(rp => rp.KitchenType)
+                   .HasConversion<string>() 
+                   .IsRequired();
+
+            builder.HasOne<Property>()
+                   .WithOne()
+                   .HasForeignKey<ResidentialProperty>(rp => rp.PropertyId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
