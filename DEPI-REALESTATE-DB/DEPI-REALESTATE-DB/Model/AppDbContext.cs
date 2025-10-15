4d9ba9514359982c3c2cc293d6728e19c3c3d1c9
Dev-Fady <fadymobiledev@gmail.com>
2025-10-15 17:33:15 +0300

    add Migration for interactions , pros

--- DEPI-REALESTATE-DB/DEPI-REALESTATE-DB/Model/AppDbContext.cs
+++ DEPI-REALESTATE-DB/DEPI-REALESTATE-DB/Model/AppDbContext.cs
@@ -9,6 +9,11 @@ namespace DEPI_REALESTATE_DB.Model
         public DbSet<Role> Roles { get; set; }
         public DbSet<Agent> Agents { get; set; }
         public DbSet<Broker> Brokers { get; set; }
+
+        public DbSet<Property> Properties { get; set; }
+        public DbSet<Wishlist> Wishlists { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Amenity> Amenities { get; set; }
         public AppDbContext() : base() { }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
@@ -19,6 +24,14 @@ namespace DEPI_REALESTATE_DB.Model
             modelBuilder.Entity<Role>().ToTable("Roles", "accounts");
             modelBuilder.Entity<Agent>().ToTable("Agents", "accounts");
             modelBuilder.Entity<Broker>().ToTable("Brokers", "accounts");
+
+            modelBuilder.Entity<Property>().ToTable("Properties", "pros");
+            modelBuilder.Entity<Amenity>().ToTable("Amenities", "pros");
+
+
+
+            modelBuilder.Entity<Wishlist>().ToTable("Wishlists", "interactions");
+            modelBuilder.Entity<Comment>().ToTable("Comments", "interactions");
 
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(RoleConfiguration).Assembly);
             base.OnModelCreating(modelBuilder);
