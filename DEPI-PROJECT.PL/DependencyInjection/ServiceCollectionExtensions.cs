86617c042cb9b7b7ed06a12291c09f32a187921c
Dev-Fady <fadymobiledev@gmail.com>
2025-10-22 16:40:42 +0300

    Add residential property services and Scalar API config

---
+++ DEPI-PROJECT.PL/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using DEPI_PROJECT.BLL.Manager.ResidentialProperty;
+using DEPI_PROJECT.BLL.Mapper;
+using DEPI_PROJECT.DAL.Repository.ResidentialProperties;
+
+namespace DEPI_PROJECT.PL.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddResidentialPropertyServices(this IServiceCollection services)
+        {
+            // Add Managers & Repositories
+            services.AddScoped<IResidentialPropertyManager, ResidentialPropertyManager>();
+            services.AddScoped<IResidentialPropertyRepo, ResidentialPropertyRepo>();
+
+            // Add AutoMapper
+            services.AddAutoMapper(typeof(ResidentialPropertyProfile));
+
+            return services;
+        }
+    }
+}
--- DEPI-PROJECT.PL/Program.cs
+++ DEPI-PROJECT.PL/Program.cs
@@ -1,5 +1,11 @@
-using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using DEPI_PROJECT.BLL.Manager.ResidentialProperty;
+using DEPI_PROJECT.BLL.Mapper;
 using DEPI_PROJECT.DAL.Models;
+using DEPI_PROJECT.DAL.Repository.ResidentialProperties;
+using DEPI_PROJECT.PL.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 namespace DEPI_PROJECT.PL
 {
     public class Program
@@ -18,6 +24,9 @@ namespace DEPI_PROJECT.PL
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
             builder.Services.AddSwaggerGen();
+            
+            builder.Services.AddResidentialPropertyServices();
+
 
             var app = builder.Build();
 
@@ -27,6 +36,13 @@ namespace DEPI_PROJECT.PL
                 app.MapOpenApi();
                 app.UseSwagger();
                 app.UseSwaggerUI();
+                app.MapScalarApiReference(options =>
+                {
+                    options.Title = "The DEPI-REALESTATE Api";
+                    options.Layout = ScalarLayout.Classic;
+                    options.HideClientButton = true;
+                    options.Theme = ScalarTheme.Saturn;
+                });
             }
 
             app.UseHttpsRedirection();
