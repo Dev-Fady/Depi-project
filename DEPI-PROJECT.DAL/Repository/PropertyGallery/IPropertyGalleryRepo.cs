9b17b2bcf4cc38ea9ed1652cb159de6671b5e34a
Dev-Fady <fadymobiledev@gmail.com>
2025-10-22 22:36:36 +0300

    Add PropertyGallery repository interface and implementation

---
+++ DEPI-PROJECT.DAL/Repository/PropertyGallery/IPropertyGalleryRepo.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DataModels = DEPI_PROJECT.DAL.Models;
+
+namespace DEPI_PROJECT.DAL.Repository.PropertyGallery
+{
+    public interface IPropertyGalleryRepo
+    {
+        IEnumerable<DataModels.PropertyGallery> GetAll();
+        IEnumerable<DataModels.PropertyGallery> GetByPropertyId(Guid propertyId);
+        void Add(DataModels.PropertyGallery gallery);
+        void Delete(Guid id);
+        DataModels.PropertyGallery GetById(Guid id);
+        void AddRange(IEnumerable<DataModels.PropertyGallery> galleries);
+    }
+}
