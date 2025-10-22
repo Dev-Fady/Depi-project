7a74889f31a13226b3a98637a1ba924a8f2b0e84
Dev-Fady <fadymobiledev@gmail.com>
2025-10-22 16:42:47 +0300

    Add AutoMapper profile for ResidentialProperty mappings
    Introduces `ResidentialPropertyProfile` class to define mappings
    between `ResidentialProperty` model and its corresponding DTOs.
    Includes custom mappings for `AgentName` and `CompoundName`,
    and ensures non-null values are mapped during updates.

---
+++ DEPI-PROJECT.BLL/Mapper/ResidentialPropertyProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using DEPI_PROJECT.BLL.DTOs.ResidentialProperty;
+using DEPI_PROJECT.DAL.Models;
+using System.Linq;
+
+namespace DEPI_PROJECT.BLL.Mapper
+{
+    public class ResidentialPropertyProfile : Profile
+    {
+        public ResidentialPropertyProfile()
+        {
+            CreateMap<ResidentialProperty, ResidentialPropertyReadDto>()
+                .ForMember(dest => dest.AgentName, opt => opt.MapFrom(src => src.Agent.Name))
+                .ForMember(dest => dest.CompoundName, opt => opt.MapFrom(src => src.Compound.Name))
+                .ForMember(dest => dest.GalleryUrls, opt => opt.MapFrom(src => src.PropertyGalleries.Select(g => g.ImageUrl)));
+
+            CreateMap<ResidentialPropertyAddDto, ResidentialProperty>();
+
+            CreateMap<ResidentialPropertyUpdateDto, ResidentialProperty>();
+
+            CreateMap<ResidentialPropertyUpdateDto, ResidentialProperty>()
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+
+        }
+    }
+}
