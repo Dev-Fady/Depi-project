943eb53e743ecc95add4a0d47c23c183c986012b
Dev-Fady <fadymobiledev@gmail.com>
2025-10-22 22:36:22 +0300

    Add PropertyGalleryServiceExtension for DI setup

---
+++ DEPI-PROJECT.PL/DependencyInjection/PropertyGalleryServiceExtension.cs
@@ -0,0 +1,15 @@
+﻿using DEPI_PROJECT.BLL.Manager.PropertyGallery;
+using DEPI_PROJECT.DAL.Repository.PropertyGallery;
+
+namespace DEPI_PROJECT.PL.DependencyInjection
+{
+    public static class PropertyGalleryServiceExtension
+    {
+        public static IServiceCollection AddPropertyGalleryServices(this IServiceCollection services)
+        {
+            services.AddScoped<IPropertyGalleryRepo, PropertyGalleryRepo>();
+            services.AddScoped<IPropertyGalleryManager, PropertyGalleryManager>();
+            return services;
+        }
+    }
+}
