1e510ab1ff30c88a334d4cc135e4e122bc17fe6d
Dev-Fady <fadymobiledev@gmail.com>
2025-10-24 11:42:12 +0300

    Add AutoMapper profile for CommercialProperty mappings
    Introduces the `CommercialPropertyProfile` class to define mapping configurations between `CommercialProperty` and various DTOs. Includes null checks for properties like `AgentName`, `CompoundName`, and `Galleries`. Sets up mappings for adding and updating commercial properties while ignoring certain properties. Also adds necessary using directives for required namespaces.

---
+++ DEPI-PROJECT.BLL/Mapper/CommercialPropertyProfile.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using DEPI_PROJECT.BLL.DTOs.CommercialProperty;
+using DEPI_PROJECT.BLL.DTOs.PropertyGallery; // تأكد إن ده مضاف
+using DEPI_PROJECT.DAL.Models;
+
+namespace DEPI_PROJECT.BLL.Mapper
+{
+    public class CommercialPropertyProfile : Profile
+    {
+        public CommercialPropertyProfile()
+        {
+            CreateMap<CommercialProperty, CommercialPropertyReadDto>()
+                .ForMember(dest => dest.AgentName, opt => opt.MapFrom(src => src.Agent != null ? src.Agent.Name : null))
+                .ForMember(dest => dest.CompoundName, opt => opt.MapFrom(src => src.Compound != null ? src.Compound.Name : null))
+                .ForMember(dest => dest.Galleries, opt => opt.MapFrom(src => src.PropertyGalleries ?? new List<PropertyGallery>()))
+                .ForMember(dest => dest.Amenity, opt => opt.MapFrom(src => src.Amenity != null ? src.Amenity : null));
+
+            CreateMap<Amenity, CommercialAmenityReadDto>();
+            CreateMap<PropertyGallery, PropertyGalleryReadDto>();
+
+            CreateMap<CommercialPropertyAddDto, CommercialProperty>()
+                .ForMember(dest => dest.Amenity, opt => opt.Ignore())
+                .ForMember(dest => dest.Agent, opt => opt.Ignore())
+                .ForMember(dest => dest.Compound, opt => opt.Ignore())
+                .ForMember(dest => dest.PropertyGalleries, opt => opt.Ignore());
+
+            CreateMap<CommercialPropertyUpdateDto, CommercialProperty>()
+                .ForMember(dest => dest.PropertyId, opt => opt.Ignore())
+                .ForMember(dest => dest.Amenity, opt => opt.Ignore())
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+            CreateMap<CommercialAmenityAddDto, Amenity>()
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+        }
+    }
+}
