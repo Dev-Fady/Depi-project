12525150b8954fede9c82cbb8c453c1a7d90d620
Dev-Fady <fadymobiledev@gmail.com>
2025-10-24 18:56:12 +0300

    Add DTO for Compound

---
+++ DEPI-PROJECT.BLL/DTOs/Compound/CompoundAddDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DEPI_PROJECT.BLL.DTOs.Compound
+{
+    public class CompoundAddDto
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ DEPI-PROJECT.BLL/DTOs/Compound/CompoundReadDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEPI_PROJECT.BLL.DTOs.Compound
+{
+    public class CompoundReadDto
+    {
+        public Guid CompoundId { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+    }
+}
