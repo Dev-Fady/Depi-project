890bdc6e00be62998f5a72c142d24d3b4d135820
Dev-Fady <fadymobiledev@gmail.com>
2025-10-24 18:55:51 +0300

    Add IManager and Imple Manager Compound

---
+++ DEPI-PROJECT.BLL/Manager/Compound/ICompoundManager.cs
@@ -0,0 +1,20 @@
+﻿using DEPI_PROJECT.BLL.DTOs.Compound;
+using DEPI_PROJECT.BLL.DTOs.Response;
+using DEPI_PROJECT.DAL.Repository.ResidentialProperties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEPI_PROJECT.BLL.Manager.Compound
+{
+    public interface ICompoundManager
+    {
+        ResponseDto<PagedResult<CompoundReadDto>> GetAllCompounds(int pageNumber, int pageSize);
+        ResponseDto<CompoundReadDto> GetCompoundById(Guid id);
+        ResponseDto<CompoundReadDto> AddCompound(CompoundAddDto compoundDto);
+        ResponseDto<bool> UpdateCompound(Guid id, CompoundUpdateDto compoundDto);
+        ResponseDto<bool> DeleteCompound(Guid id);
+    }
+}
